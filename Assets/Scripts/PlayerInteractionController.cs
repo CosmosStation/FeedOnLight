3e19eb5ee7450ee60445b9d095dedc9161f11e02
Georgy Dolganov <mr.craaazy@gmail.com>
2022-08-16 10:59:00 +0300

    Playground and player prefab update

--- Assets/Scripts/PlayerInteractionController.cs
+++ Assets/Scripts/PlayerInteractionController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerInteractionController : MonoBehaviour
 {
@@ -9,18 +10,10 @@ public class PlayerInteractionController : MonoBehaviour
 
     [SerializeField] private float interactRange = 30f;
     [SerializeField] private GameObject hand;
+    [SerializeField] private Camera camera;
 
     private RaycastHit currentHit;
     private bool isReadyToInteract = false;
-
-    [Header("PICKUP PARAMETERS")] 
-    [SerializeField] private Transform holdArea;
-
-    [SerializeField] private float pickupRange = 5.0f;
-    [SerializeField] private float pickupForce = 150.0f;
-
-    private GameObject heldObj;
-    private Rigidbody heldObjRB;
     
     void Start()
     {
@@ -29,6 +22,39 @@ public class PlayerInteractionController : MonoBehaviour
     
     void FixedUpdate()
     {
+        RaycastHit hit;
         
+        Transform cameraTransform = camera.transform;
+        
+        if (Physics.Raycast(cameraTransform.position, cameraTransform.forward, out hit, interactRange, layerMaskInteract.value))
+        {
+            Debug.DrawRay(cameraTransform.position, cameraTransform.forward * hit.distance, Color.green);
+            // Debug.Log("Ray hit");
+            currentHit = hit;
+            hand.transform.localEulerAngles = new Vector3(0, -13, 0);
+            isReadyToInteract = true;
+        }
+        else
+        {
+            Debug.DrawRay(cameraTransform.position, cameraTransform.forward * interactRange, Color.red);
+            // Debug.Log("Ray did not hit");
+            hand.transform.localEulerAngles = new Vector3(45, -13, 0);
+            isReadyToInteract = false;
+        }
     }
+    
+    public void Interact(InputAction.CallbackContext context)
+    {
+        if (!isReadyToInteract || context.phase != InputActionPhase.Performed) return;
+        Debug.Log(currentHit);
+        switch (currentHit.transform.tag)
+        {
+            case "AbsorbItem":
+                Debug.Log("Absorbing item");
+                GameObject gameObj = currentHit.transform.gameObject;
+                Debug.Log(gameObj);
+                break;
+        }
+    }
+    
 }
