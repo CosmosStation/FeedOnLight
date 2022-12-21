d77909d446bffa91eacfb2a7bbd0e2fcea37ea58
Georgy Dolganov <mr.craaazy@gmail.com>
2022-12-21 20:57:26 +0400

    Patrol Enemy now follows the player on level 1

--- Assets/Scripts/AI/PatrolSightSensor.cs
+++ Assets/Scripts/AI/PatrolSightSensor.cs
@@ -1,44 +1,49 @@
-﻿using System;
+﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace PatrolEnemy
 {
     public class PatrolSightSensor : MonoBehaviour
     {
-        public Transform Player { get; private set; }
+    public Transform Player { get; private set; }
 
-        [SerializeField] private LayerMask _ignoreMask;
+    [SerializeField] private LayerMask _ignoreMask;
 
-        private Ray _ray;
+    private Ray _ray;
 
-        private void Awake()
+    private void Awake()
+    {
+        Player = GameObject.Find("PlayerCapsule").transform;
+    }
+
+    public bool Ping()
+    {
+        if (Player == null)
+            return false;
+
+        _ray = new Ray(this.transform.position, Player.position - this.transform.position);
+
+        if (Vector3.Dot(_ray.direction, this.transform.forward) < 0)
+            return false;
+
+        if (!Physics.Raycast(_ray, out var hit, 100, ~_ignoreMask))
         {
-            Player = GameObject.Find("Player").transform;
-        }
-
-        public bool Ping()
-        {
-            if (Player == null)
-                return false;
-
-            _ray = new Ray(this.transform.position, Player.position - this.transform.position);
-
-            if (Vector3.Dot(_ray.direction, this.transform.forward) < 0)
-                return false;
-
-            if (!Physics.Raycast(_ray, out var hit, 100, ~_ignoreMask))
-                return false;
-
-            if (hit.collider.tag == "Player")
-                return false;
-            
             return false;
         }
-        
-        private void OnDrawGizmos()
+
+        if (hit.collider.tag == "Player")
         {
-            Gizmos.color = Color.red;
-            Gizmos.DrawLine(_ray.origin, _ray.origin + _ray.direction * 100);
+            return true;
         }
+
+        return false;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(_ray.origin, _ray.origin + _ray.direction * 100);
+    }
     }
 }
