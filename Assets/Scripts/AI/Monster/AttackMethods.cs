cf9f2ab1611ca5818e18e698aa84e9ad7af7b3b4
Georgy Dolganov <mr.craaazy@gmail.com>
2023-05-02 23:10:31 +0400

    Grabbing state update

---
+++ Assets/Scripts/AI/Monster/AttackMethods.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackMethods : MonoBehaviour
+{
+    public bool isInRange()
+    {
+        return false;
+    }
+    
+    void Grab()
+    {
+    }
+}
