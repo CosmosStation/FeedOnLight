8619950e6705b90c1d33e44a5c4bad9f57b0f33d
Ksenia <42271856+youshaSilence@users.noreply.github.com>
2023-05-24 21:50:19 +0400

    Камера не двигается, когда игра на паузе

--- Assets/Scripts/InventoryController.cs
+++ Assets/Scripts/InventoryController.cs
@@ -1,17 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using StarterAssets;
 
 public class InventoryController : MonoBehaviour
 {
+    [Header("PlayerController")] public FirstPersonController PersonController;
+
 
     [Header("Inventory")] [SerializeField] private GameObject _inventory;
 
     public void OpenInventory()
     {
-        Debug.Log("Open Inventory");
         _inventory.SetActive(true);
         Cursor.visible = true;
         Cursor.lockState = CursorLockMode.Confined;
+        PersonController.LockCamera();
     }
 }
