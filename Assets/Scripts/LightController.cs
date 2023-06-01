cb08d66808a25d9e153d830c719a37310eba4297
Ksenia <42271856+youshaSilence@users.noreply.github.com>
2023-06-01 19:56:25 +0400

    Добавлены все шприцы как айтемы

--- Assets/Scripts/LightController.cs
+++ Assets/Scripts/LightController.cs
@@ -12,10 +12,10 @@ public class LightController : MonoBehaviour
     {
         colors.Add("white", Color.white);
         colors.Add("blue", Color.blue);
-        colors.Add("pink", new Color(255, 105, 180));
+        colors.Add("pink", new Color(1, 0.427451f, 0.8268133f));
         colors.Add("yellow", Color.yellow);
         colors.Add("green", Color.green);
-        colors.Add("lilac", new Color(210, 175, 255));
+        colors.Add("lilac", new Color(0.6320614f, 0.427451f, 1));
     }
 
     public void Turn()
